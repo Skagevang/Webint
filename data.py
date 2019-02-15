7bfa4fd1c1b7b71a869f74372e15ec6506cb6f79
yujie-xing <yujie.xing@ntnu.no>
2019-02-15 18:10:17 +0100

    updated with correct evaluation method

--- data.py
+++ data.py
@@ -2,13 +2,16 @@ import os
 import json
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,recall_score
 
 class data:
 
 	def __init__(self, path):
 		self.origin_data=self.read_data(path)
-		self.status="original"
+		self.remove_frontpage()
+		self.gen_click_matrix()
+		self.gen_question_key()
+
 
 	def read_data(self, path):
 		"""
@@ -33,47 +36,12 @@ class data:
 		self.origin_data=self.origin_data[self.origin_data["documentId"].notnull()]
 		self.status="frontpage removed"
 
-	def remove_nan(self,category="userId"):
-		"""
-		'nan' values removed according to given category
-		parameters: "all" or category's name or list
-		stored in self.data
-		type: pandas
-		"""
-		if category=="all":
-			self.data=self.origin_data.dropna(how="any")
-			self.status="all none removed"
-		elif type(category)==str:
-			self.data = self.origin_data[self.origin_data[category].notna()]
-			self.status=category+" removed"
-		else:
-			self.data=self.origin_data.dropna(how="any",subset=category)
-			self.status=", ".join(category)+" removed"
-
-	def show_statistics(self):
-	    """
-	    print statistics of current self.data
-	    """
-	    df=self.data.sort_values(by=['userId', 'time'], ascending=True)
-	    num=df.shape[0]
-	    print("Number of remaining events: {}".format(num))
-	    num_docs = df['documentId'].nunique()	    
-	    print("Number of remaining documents: {}".format(num_docs))
-	    print('Sparsity: {:4.3f}%'.format(float(num) / float(1000*num_docs) * 100))
-	    df.drop_duplicates(subset=['userId', 'documentId'], inplace=True)
-	    print("Number of remaining events(drop duplicates): {}".format(df.shape[0]))
-	    print('Sparsity (drop duplicates): {:4.3f}%'.format(float(df.shape[0]) / float(1000*num_docs) * 100))
-	    
-	    user_df = df.groupby(['userId']).size().reset_index(name='counts')
-	    print("Describe by user:")
-	    print(user_df.describe())
-
 	def numbering(self, df):
 		"""
 		add two columns: uid and tid
 		which transffer userID and documentID into integers starting from 1
 		"""
-		df = df.sort_values(by=['userId', 'documentId'])
+		df = df.sort_values(by=['userId', 'time'])
 		new_user = df['userId'].values[1:] != df['userId'].values[:-1]
 		new_user = np.r_[True, new_user]
 		df['uid'] = np.cumsum(new_user)
@@ -82,14 +50,13 @@ class data:
 		df_merge = pd.merge(df, new_df, on='documentId', how='outer')
 		return df_merge
 
-
 	def gen_click_matrix(self):
 		"""
-		generate click matrix based on self.data
+		generate click matrix on self.origin_data
 		stored in self.click_matrix
 		type: numpy.arrays
 		"""
-		df=self.data.drop_duplicates(subset=['userId', 'documentId'])
+		df=self.origin_data.drop_duplicates(subset=['userId', 'documentId'])
 		n_users = df['userId'].nunique()
 		n_items = df['documentId'].nunique()
 		df_merge=self.numbering(df)
@@ -98,38 +65,100 @@ class data:
 		for row in df_ext.itertuples():
 			self.click_matrix[row[1]-1, row[2]-1] = 1.0
 
-	def gen_time_matrix(self):
+		self.data=pd.merge(self.origin_data,df_merge[['userId','documentId','uid','tid']],on=['userId','documentId'],how='outer').sort_values(by=['userId','time'])
+
+	def gen_question_key(self,fraction=0.2):
 		"""
-		generate activetime matrix based on self.data
-		stored in self.time_matrix
-		type: numpy.arrays
+		Erase 20% values from the click matrix, in order to do testing.
 		"""
-		if 'activeTime' not in self.status:
-			raise("remove nan values in activeTime first!")
-		df=self.data[['userId','documentId','activeTime']].groupby(['userId','documentId'],as_index=False).sum()
-		n_users = df['userId'].nunique()
-		n_items = df['documentId'].nunique()
-		df_merge=self.numbering(df)
-		df_ext = df_merge[['uid', 'tid', 'activeTime']]
-		self.time_matrix = np.zeros((n_users, n_items))
-		for row in df_ext.itertuples():
-			self.time_matrix[row[1]-1, row[2]-1] = row[3] 
+		self.question = self.click_matrix.copy()
+		self.key = np.zeros(self.click_matrix.shape)
+		location = np.zeros(self.click_matrix.shape)
+		for user in range(self.click_matrix.shape[0]):
+			size = int(len(self.click_matrix[user, :].nonzero()[0]) * fraction)
+			test_ratings = np.random.choice(self.click_matrix[user, :].nonzero()[0], 
+											size=size, 
+											replace=False)
+			self.question[user, test_ratings] = -1
+			self.key[user, test_ratings] = self.click_matrix[user, test_ratings]
+			location[user, test_ratings] = 1
+		self.location=location.nonzero()
+
+	def evaluate(self,pred):
+		pred = pred[self.location].flatten()
+		key = self.key[self.location].flatten()
+		print("MSE is {:.4f}".format(mean_squared_error(pred, key)))
+		print("Recall is {:.4f}".format(recall_score(key, pred)))
+
+	def show_statistics(self):
+		"""
+		print statistics of current self.data
+		"""
+		df=self.data
+		num=df.shape[0]
+		print("Number of events: {}".format(num))
+		num_docs = df['documentId'].nunique()	    
+		print("Number of documents: {}".format(num_docs))
+		print('Sparsity: {:4.3f}%'.format(float(num) / float(1000*num_docs) * 100))
+		df.drop_duplicates(subset=['userId', 'documentId'], inplace=True)
+		print("Number of events(drop duplicates): {}".format(df.shape[0]))
+		print('Sparsity (drop duplicates): {:4.3f}%'.format(float(df.shape[0]) / float(1000*num_docs) * 100))
+		
+		user_df = df.groupby(['userId']).size().reset_index(name='counts')
+		print("Describe by user:")
+		print(user_df.describe())
+
+	# def remove_nan(self,category="userId"):
+	# 	"""
+	# 	'nan' values removed according to given category
+	# 	parameters: "all" or category's name or list
+	# 	stored in self.data
+	# 	type: pandas
+	# 	"""
+	# 	if category=="all":
+	# 		self.data=self.origin_data.dropna(how="any")
+	# 		self.status="all none removed"
+	# 	elif type(category)==str:
+	# 		self.data = self.origin_data[self.origin_data[category].notna()]
+	# 		self.status=category+" removed"
+	# 	else:
+	# 		self.data=self.origin_data.dropna(how="any",subset=category)
+	# 		self.status=", ".join(category)+" removed"
 
 
-	def train_test_split(self,matrix,fraction=0.2,valid=False):
-		"""
-		train test split, based on given matrix
-		return 2 or 3 matrix
-		"""
-		m=matrix.copy()
-		np.random.shuffle(m)
-		l=m.shape[0]
-		if valid:
-			train=m[:int(l*(1-2*fraction))]
-			valid=m[int(l*(1-2*fraction)):int(l*(1-fraction))]
-			test=m[int(l*(1-fraction)):]
-			return train, valid, test
-		else:
-			train=m[:int(l*(1-fraction))]
-			test=m[int(l*(1-fraction)):]
-			return train, test
+
+	# def gen_click_cum_matrix(self):
+	# 	"""
+	# 	generate accumulated click matrix based on self.data
+	# 	stored in self.click_cum_matrix
+	# 	type: numpy.arrays
+	# 	"""
+	# 	df=self.data[['userId','documentId']]
+	# 	df["click"]=np.ones(self.data.shape[0])
+	# 	df=df.groupby(['userId','documentId'],as_index=False).sum()
+	# 	n_users = df['userId'].nunique()
+	# 	n_items = df['documentId'].nunique()
+	# 	df_merge=self.numbering(df)
+	# 	df_ext = df_merge[['uid', 'tid', 'click']]
+	# 	self.click_cum_matrix = np.zeros((n_users, n_items))
+	# 	for row in df_ext.itertuples():
+	# 		self.click_cum_matrix[row[1]-1, row[2]-1] = row[3]
+	# 	return df_merge
+
+	# def gen_time_matrix(self):
+	# 	"""
+	# 	generate activetime matrix based on self.data
+	# 	stored in self.time_matrix
+	# 	type: numpy.arrays
+	# 	"""
+	# 	if 'activeTime' not in self.status:
+	# 		raise("remove nan values in activeTime first!")
+	# 	df=self.data[['userId','documentId','activeTime']].groupby(['userId','documentId'],as_index=False).sum()
+	# 	n_users = df['userId'].nunique()
+	# 	n_items = df['documentId'].nunique()
+	# 	df_merge=self.numbering(df)
+	# 	df_ext = df_merge[['uid', 'tid', 'activeTime']]
+	# 	self.time_matrix = np.zeros((n_users, n_items))
+	# 	for row in df_ext.itertuples():
+	# 		self.time_matrix[row[1]-1, row[2]-1] = row[3] 
+	# 	return df_merge
--- test.py
+++ test.py
@@ -1,48 +1,33 @@
 from data import *
 
-print("started. It may take some time.")
+print("Started. It may take some time.")
 print("===========")
 
 path="active1000"
 dataset=data(path)
 
-dataset.remove_frontpage()
-print("The number of events after removing frontpage:")
-print(dataset.origin_data.shape[0])
-print("===========")
-### Use dataset.origin_data to get these data.
+print("Shape of the original data:")
+print(dataset.origin_data.shape)
 
-dataset.remove_nan(["userId","activeTime"])
-print("The number of events after removing nan in userId and activeTime:")
-print(dataset.data.shape[0])
-print("The status of the cleared data:")
-print(dataset.status)
-print("===========")
-### Use dataset.data to get the cleared data.
-### Use dataset.status to check the current status of the data.
+print("Shape of the numberred data:")
+print(dataset.data.shape)
+print("Columns contain:")
+print(dataset.data.columns)
 
-dataset.gen_click_matrix()
-print("The shape of the click matrix:")
-print(dataset.click_matrix.shape)
-print("The status is still:")
-print(dataset.status)
-print("===========")
-### Use dataset.click_matrix to get the click matrix.
-
-dataset.gen_time_matrix()
-print("The shape of the time matrix:")
-print(dataset.time_matrix.shape)
-print("The status is still:")
-print(dataset.status)
-print("===========")
-### Use dataset.time_matrix to get the time matrix.
-
-train,valid,test=dataset.train_test_split(dataset.click_matrix,valid=True)
-print("The shape of the train, valid and test sets are:")
-print(train.shape)
-print(valid.shape)
-print(test.shape)
 print("===========")
 
-print("Show some statistics.")
-dataset.show_statistics()
+print("Overview of the click matrix:")
+print(dataset.click_matrix[:5,:5])
+
+print("Overview of the question matrix:")
+print(dataset.question[:5,:5])
+
+print("Overview of the key matrix:")
+print(dataset.key[:5,:5])
+
+print("See the location of the questions:")
+print(dataset.location)
+
+print("Evaluate:")
+pred=dataset.click_matrix
+dataset.evaluate(pred)
